4a6f908228c713d942e15873ccc0d29a7037f240
seokkeu <65849031+seokkeu@users.noreply.github.com>
2022-11-07 17:15:50 +0900

    Add files via upload
    /etc/pam.d/ssh
    echo 'session    optional     pam_exec.so /etc/pam.d/Alert.sh' >> /etc/pam.d/sshd

---
+++ sessionAlert.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# sessionAlert.sh_Update_2022-10-31
+# Dev:seokkeu00@gmail.com
+
+SYSLOGSERVER="x.x.x.x" 
+SRV_HOSTNAME=$(hostname -f)
+SRV_IP=$(hostname -I | awk '{print $1}')
+PROXY="http://x.x.x.x"
+
+if [ "$PAM_TYPE" == "open_session" ]; then
+    COUNTRY=$(curl --proxy $PROXY -s ipinfo.io/${PAM_RHOST} | awk -F ':' '/country/ {print $2}' | sed 's/"//gi' | sed 's/,//gi')
+    TEXT="$SRV_IP($SRV_HOSTNAME) Connect ssh session / User=${PAM_USER} / Client IP : ${PAM_RHOST}($COUNTRY)"
+    logger -n $SYSLOGSERVER ${TEXT}
+elif [ "$PAM_TYPE" == "close_session" ]; then
+    COUNTRY=$(curl --proxy $PROXY -s ipinfo.io/${PAM_RHOST} | awk -F ':' '/country/ {print $2}' | sed 's/"//gi' | sed 's/,//gi')
+    TEXT="$SRV_IP($SRV_HOSTNAME) Terminate ssh session / User=${PAM_USER} / Client IP ${PAM_RHOST}($COUNTRY)"
+    logger -n $SYSLOGSERVER ${TEXT}
+else
+    COUNTRY=$(curl --proxy $PROXY -s ipinfo.io/${PAM_RHOST} | awk -F ':' '/country/ {print $2}' | sed 's/"//gi' | sed 's/,//gi')
+    TEXT="$SRV_IP($SRV_HOSTNAME) Terminate ssh session / User=${PAM_USER} / Client IP ${PAM_RHOST}($COUNTRY)"
+    logger -n $SYSLOGSERVER ${TEXT}
+fi
+exit
